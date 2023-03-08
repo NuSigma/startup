5fa373f94b8fd38ecbb8146f4e1e7923c66349ad
Dylan Bailey <dbbailey97@gmail.com>
2023-03-08 16:12:14 -0700

    login.js creation with todo

---
+++ login.js
@@ -0,0 +1,12 @@
+// Simon example login function
+// function login() {
+//   const nameEl = document.querySelector("#name");
+//   localStorage.setItem("userName", nameEl.value);
+//   window.location.href = "play.html";
+// }
+
+// TODO: (Don't think I yet can do with just JS)
+// login validation
+// login passthru
+// email verifiection
+// user specific page instantiation 
